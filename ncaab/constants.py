15cdd65fc0b1e7d27f3865f1bd99b44c084e7325
rileypeterson <rileypeterson21@gmail.com>
2022-03-18 13:32:49 -0700

    9 merge last and next events into last events (#14)
    * Progress on #9
    
    * GH ACTION scrape Fri Mar 18 09:10:09 UTC 2022
    
    * Sync next and last dfs
    
    * GH ACTION scrape Fri Mar 18 10:10:48 UTC 2022
    
    * GH ACTION scrape Fri Mar 18 11:08:48 UTC 2022
    
    * GH ACTION scrape Fri Mar 18 12:15:50 UTC 2022
    
    * GH ACTION scrape Fri Mar 18 13:16:49 UTC 2022
    
    * GH ACTION scrape Fri Mar 18 16:13:21 UTC 2022
    
    * Stop cron yaml
    
    * Merger code
    
    * Update workflow
    
    * GH ACTION scrape Fri Mar 18 13:26:27 PDT 2022
    
    * Dataframe safety check
    
    Co-authored-by: github-actions <actions@github.com>

---
+++ ncaab/constants.py
@@ -0,0 +1,18 @@
+BOVADA_COLUMNS = [
+    "scrape_datetime",
+    "game_datetime",
+    "top_team",
+    "top_final",
+    "bottom_final",
+    "bottom_team",
+    "top_spread",
+    "top_spread_odds",
+    "top_ml_odds",
+    "top_total",
+    "top_total_odds",
+    "bottom_spread",
+    "bottom_spread_odds",
+    "bottom_ml_odds",
+    "bottom_total",
+    "bottom_total_odds",
+]
