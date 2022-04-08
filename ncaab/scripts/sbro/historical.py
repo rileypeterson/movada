62087f75bdaf81c569d847a1ea37951b358d09f5
Riley Peterson <rileypeterson21@gmail.com>
2022-04-08 00:50:30 -0700

    Reorganize

--- ncaab/scripts/sbro/historical.py
+++ ncaab/scripts/sbro/historical.py
@@ -7,11 +7,15 @@ import glob
 import os
 import pathlib
 import numpy as np
-from ncaab.constants import BOVADA_COLUMNS
+from ncaab.constants import BOVADA_COLUMNS, ROOT_DIR
 
 
 if __name__ == "__main__":
-    dir_path = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
+    dir_path = pathlib.Path(
+        os.path.dirname(
+            os.path.join(ROOT_DIR, "ncaab/data/odds/sbro/ncaa basketball 2012-13.xlsx")
+        )
+    )
     pat = os.path.join(dir_path, "ncaa basketball 20??-??.xlsx")
     files = sorted(glob.glob(pat))[::-1]
     dfs = []
@@ -119,5 +123,6 @@ if __name__ == "__main__":
     master_df.rename(columns={"game_date": "game_datetime"}, inplace=True)
     master_df = master_df[BOVADA_COLUMNS]
     master_df.to_csv(
-        os.path.join(os.path.dirname(file), "ncaab_history_init.csv"), index=False
+        os.path.join(ROOT_DIR, "ncaab/data/odds/sbro/ncaab_history_init.csv"),
+        index=False,
     )
--- ncaab/scripts/sbro/past_events.py
+++ ncaab/scripts/sbro/past_events.py
@@ -2,8 +2,6 @@ import pandas as pd
 import os
 from ncaab.constants import ROOT_DIR
 from ncaab.utils.teams import teams
-import time
-import random
 
 skips = [
     "Chaminade",
@@ -14,8 +12,8 @@ skips = [
 ]
 
 if __name__ == "__main__":
-    sbro_path = os.path.join(ROOT_DIR, "ncaab/data/sbro/ncaab_history_init.csv")
-    past_path = os.path.join(ROOT_DIR, "ncaab/data/sbro/past_events.csv")
+    sbro_path = os.path.join(ROOT_DIR, "ncaab/data/odds/sbro/ncaab_history_init.csv")
+    past_path = os.path.join(ROOT_DIR, "ncaab/data/odds/sbro/past_events.csv")
     sbro_df = pd.read_csv(sbro_path)
     for i in range(len(sbro_df)):
         top_team = sbro_df.loc[i, "top_team"]
@@ -26,12 +24,6 @@ if __name__ == "__main__":
                 c = True
         if c:
             continue
-        # for tt, ttt in replacements:
-        #     if top_team == tt:
-        #         top_team = ttt
-        #     if bottom_team == tt:
-        #         bottom_team = ttt
         sbro_df.loc[i, "top_team"] = teams[top_team]["srcbb_school"]
-        print(bottom_team)
         sbro_df.loc[i, "bottom_team"] = teams[bottom_team]["srcbb_school"]
     sbro_df.to_csv(past_path)
