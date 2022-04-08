1f494fac666811d359db551ab9e434872457bd39
Riley Peterson <rileypeterson21@gmail.com>
2022-04-08 14:34:43 -0700

    Combine data

--- ncaab/scripts/combine_data.py
+++ ncaab/scripts/combine_data.py
@@ -6,16 +6,17 @@ if __name__ == "__main__":
     past_df = pd.read_csv(
         os.path.join(ROOT_DIR, "ncaab/data/odds_with_stats/sbro/past_events.csv"),
         index_col=0,
+        dtype=str,
     )
     last_df = pd.read_csv(
         os.path.join(ROOT_DIR, "ncaab/data/odds_with_stats/bovada/last_events.csv"),
         index_col=0,
+        dtype=str,
     )
     df = (
         pd.concat([last_df, past_df], axis=0)
-        .sort_values("game_datetime", ascending=False)
+        .sort_values(["game_datetime", "top_team"], ascending=False)
         .reset_index(drop=True)
     )
-
-    # to csv dtype str
-    a = 1
+    dataset_path = os.path.join(ROOT_DIR, "ncaab/data/dataset.csv")
+    df.to_csv(dataset_path)
